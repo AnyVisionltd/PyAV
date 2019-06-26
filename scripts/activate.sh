e330ee619456101d2088a7e047fa2b72ee275a00
Ziv Tsarfati <digger18@gmail.com>
2019-06-26 09:49:50 +0300

    Removed virtualenv crap from scripts/activate.sh

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -30,49 +30,8 @@ if [[ ${#_lib_parts[@]} != 2 ]]; then
 fi
 export PYAV_LIBRARY_NAME=${_lib_parts[0]}
 export PYAV_LIBRARY_VERSION=${_lib_parts[1]}
-
-
-if [[ ! "$PYAV_PYTHON" ]]; then
-    PYAV_PYTHON="${PYAV_PYTHON-python3}"
-    echo 'No $PYAV_PYTHON set; defaulting to python3.'
-fi
-export PYAV_PYTHON
-export PYAV_PIP="${PYAV_PIP-$PYAV_PYTHON -m pip}"
-
-if [[ "$TRAVIS" ]]; then
-
-    # Travis as a very self-contained environment. Lets just work in that.
-    echo "We're on Travis, so not setting up another virtualenv."
-
-    if [[ "$TRAVIS_PYTHON_VERSION" = "2.7" || "$TRAVIS_PYTHON_VERSION" = "pypy" ]]; then
-        PYAV_PYTHON=python
-        PYAV_PIP=pip
-    fi
-else
-
-    export PYAV_VENV_NAME="$(uname -s).$(uname -r).$("$PYAV_PYTHON" -c '
-from __future__ import print_function
-import sys
-import platform
-print("{}{}.{}".format(platform.python_implementation().lower(), *sys.version_info[:2]))
-    ')"
-    export PYAV_VENV="$PYAV_ROOT/venvs/$PYAV_VENV_NAME"
-
-    if [[ ! -e "$PYAV_VENV/bin/python" ]]; then
-        mkdir -p "$PYAV_VENV"
-        virtualenv -p "$PYAV_PYTHON" "$PYAV_VENV"
-        "$PYAV_VENV/bin/pip" install --upgrade pip setuptools
-    fi
-
-    if [[ -e "$PYAV_VENV/bin/activate" ]]; then
-        source "$PYAV_VENV/bin/activate"
-    else
-        # Not a virtualenv (perhaps a debug Python); lets manually "activate" it.
-        PATH="$PYAV_VENV/bin:$PATH"
-    fi
-
-fi
-
+export PYAV_PYTHON=python
+export PYAV_PIP=pip
 
 # Just a flag so that we know this was supposedly run.
 export _PYAV_ACTIVATED=1
